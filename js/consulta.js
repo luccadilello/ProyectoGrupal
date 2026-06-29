563be49752874d256d220ad7908b7c02a1bbd923
luccadilello <ldilello@uade.edu.ar>
2026-06-28 22:27:07 -0300

    Update consulta.js

--- js/consulta.js
+++ js/consulta.js
@@ -52,3 +52,47 @@ const destinos = {
     frecuencia: "15-20 minutos"
   }
 };
+const boton = document.getElementById("buscar");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", () => {
+
+    const destino = document
+        .getElementById("destino")
+        .value
+        .toLowerCase()
+        .trim();
+
+    const info = destinos[destino];
+
+    resultado.style.display = "block";
+
+    if (!info) {
+        resultado.innerHTML = `
+            <h3>❌ No se encontró el destino</h3>
+            <p>Probá con:</p>
+            <ul>
+                <li>Liniers</li>
+                <li>Aeroparque</li>
+                <li>Constitución</li>
+                <li>Ezeiza estacion</li>
+            </ul>
+        `;
+        return;
+    }
+
+    resultado.innerHTML = `
+        <h3>✔ Resultado encontrado</h3>
+
+        <p><b>Servicio:</b> ${info.servicio}</p>
+        <p><b>Plataforma:</b> ${info.plataforma}</p>
+        <p><b>Tiempo estimado:</b> ${info.tiempo}</p>
+        <p><b>Costo:</b> ${info.costo}</p>
+        <p><b>Frecuencia:</b> ${info.frecuencia}</p>
+
+        <h4>Accesibilidad</h4>
+        <ul>
+            ${info.accesibilidad.map(a => `<li>${a}</li>`).join("")}
+        </ul>
+    `;
+});
